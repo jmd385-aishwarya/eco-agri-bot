e62e788cd33640c348161ebfd52b24583cf3f6ee
Aishwarya Shree H <aishwarya.h@jmangroup.com>
2026-05-18 19:08:25 +0530

    fix: collectstatic error

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t)y6e+d$=h2gi1-c%@(-m80tgvpqg^o)!-@5@0-%a%)t397fa('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
